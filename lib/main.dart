8362d3b64221f43d82f3d713948c7ef1284b909b
unknown <nadirhemed@gmail.com>
2020-10-06 10:55:37 +0300

    Quiz app with flutter and dart complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quizbrain.dart';
+
 
 void main() => runApp(Quizzler());
 
+
 class Quizzler extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,6 +29,12 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreTracer = [];
+
+  QuizBrain quizBrain = QuizBrain();
+
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +71,30 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer = quizBrain.getQuestionAnswer();
+                if(correctAnswer == true){
+                  setState(() {
+                    scoreTracer.add(
+                        Icon(
+                            Icons.check,
+                            color: Colors.green
+                        )
+                    );
+                  });
+                }else{
+                  setState(() {
+                    scoreTracer.add(
+                        Icon(
+                            Icons.close,
+                            color: Colors.red
+                        )
+                    );
+                  });
+                }
+
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
@@ -79,12 +112,38 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer = quizBrain.getQuestionAnswer();
+                if(correctAnswer == false){
+                    setState(() {
+                      scoreTracer.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green
+                        )
+                      );
+                    });
+                }else{
+                  setState(() {
+                    scoreTracer.add(
+                        Icon(
+                            Icons.close,
+                            color: Colors.red
+                        )
+                    );
+                  });
+                }
+                setState(() {
+                  quizBrain.nextQuestion() ;
+
+                });
+
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreTracer,
+        ),
       ],
     );
   }
